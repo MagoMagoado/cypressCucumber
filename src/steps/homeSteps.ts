47c0068038127b6f60ed89a8618a0e23f8654eb3
Isabella <IsabellaCarolina.SilvadeAraujo@thomsonreuters.com>
2026-06-22 13:56:21 -0300

    Revert "primeiro commit"
    This reverts commit 8ae316ea4bddd193acb83ae8da112703614e6162.

--- src/steps/homeSteps.ts
+++
@@ -1,6 +0,0 @@
-import { When } from '@fixtures/fixtures';
-
-When('que deslizo o slider {string} para mínimo {string} e máximo {string}', async ({ homePage }, nome: string, valorMin: string, valorMax: string) => {
-  await homePage.deslizarSlider(nome, valorMin, valorMax);
-});
-
