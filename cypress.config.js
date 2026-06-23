96da4d2ab291d1f06b28c3fe97904d5ab484348c
mm <isabella-arj@hotmail.com>
2026-06-23 00:54:39 -0300

    arrumando projeto

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,14 @@
-const { defineConfig } = require('cypress');
-const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
-const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
-const createBundler = require('@bahmutov/cypress-esbuild-bundler');
-
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 module.exports = defineConfig({
   e2e: {
-    specPattern: 'features/**/*.feature',
-    supportFile: 'cypress/support/e2e.js',
-    defaultCommandTimeout: 60000,
-    pageLoadTimeout: 60000,
-    screenshotsFolder: 'test-results/screenshots',
-    videosFolder: 'test-results/videos',
-    video: false,
-    screenshotOnRunFailure: true,
+    specPattern: "**/*.feature",
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
       on(
-        'file:preprocessor',
+        "file:preprocessor",
         createBundler({
           plugins: [createEsbuildPlugin(config)],
         })
