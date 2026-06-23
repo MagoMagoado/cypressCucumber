802a2cb615504343ea1fb971321a90b5b5e782a1
Isabella <IsabellaCarolina.SilvadeAraujo@thomsonreuters.com>
2026-06-23 01:39:32 -0300

    arrumando viewport

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 module.exports = defineConfig({
+  viewportWidth: 1440,
+  viewportHeight: 900,
   e2e: {
     specPattern: "cypress/e2e/features/**/*.feature",
     async setupNodeEvents(on, config) {
